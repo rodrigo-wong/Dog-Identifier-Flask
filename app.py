640d49a1c33efcaa7db587f57e97e85c68e3fe74
rodrigo-wong <wongmacr@gmail.com>
2025-01-26 03:35:13 -0500

    Update app to run on specified PORT and allow external access

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ def predict_breed():
         os.rmdir(temp_dir)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
